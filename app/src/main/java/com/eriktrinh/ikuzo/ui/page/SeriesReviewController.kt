5542b9025cd44ec632201dbaf9071249ceb2a80d
Erik Trinh <trinh.erik@gmail.com>
2016-11-17 01:30:53 -0500

    Setup viewpager

--- app/src/main/java/com/eriktrinh/ikuzo/ui/page/SeriesReviewController.kt
+++ app/src/main/java/com/eriktrinh/ikuzo/ui/page/SeriesReviewController.kt
@@ -7,10 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.eriktrinh.ikuzo.R
 import com.eriktrinh.ikuzo.data.ani.Anime
+import com.eriktrinh.ikuzo.data.ani.Favourite
 import com.eriktrinh.ikuzo.data.ani.Record
-import com.eriktrinh.ikuzo.data.enums.ListStatus
 import com.eriktrinh.ikuzo.data.payload.Id
 import com.eriktrinh.ikuzo.ui.SpacingItemDecoration
 import com.eriktrinh.ikuzo.utils.ext.loadAndCropInto
@@ -19,11 +20,21 @@ import com.eriktrinh.ikuzo.web.service.SeriesService
 import com.jaredrummler.materialspinner.MaterialSpinner
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.controller_series_page.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class SeriesPageController(args: Bundle?) : PagerChildController(args) {
+/**
+ *  TODO:
+ *  - Display the initial reviews (in a list)
+ *  - 'Load More' button at the bottom of the list
+ *  - FAB to write a review (implemented later)
+ *  - click on reviewers profile, vote on review (later)
+ */
+class SeriesReviewController(args: Bundle?) : PagerChildController(args) {
 
     companion object {
-        private val TAG = "SeriesPageController"
+        private val TAG = "SeriesReviewController"
         private val KEY_ID = "ARGS_ID"
         private fun Bundle.putId(id: Int): Bundle {
             this.putInt(KEY_ID, id)
@@ -79,69 +90,22 @@ class SeriesPageController(args: Bundle?) : PagerChildController(args) {
 
     private fun initButtons() {
         favFab.setImageResource(R.drawable.ic_menu_unfavourited)
-        favFab.setOnClickListener { getPresenter().onFavouriteButtonClicked(activity) }
-    }
+        favFab.setOnClickListener { view ->
+            val call = seriesService.favAnime(Id(id))
+            call.enqueue(object : Callback<Favourite> {
+                override fun onResponse(call: Call<Favourite>, response: Response<Favourite>?) {
+                    if (response != null && response.code() == 200) {
+                        favFab.setImageResource(if (response.body().order == null) R.drawable.ic_menu_favourited else R.drawable.ic_menu_unfavourited)
+                    } else {
+                        Toast.makeText(activity, "Could not update favourite", Toast.LENGTH_SHORT)
+                                .show()
+                    }
+                }
 
-    private fun initSpinners(series: Anime, status: Record?) {
-        val updateButton = view.series_detail_update_button
-        val posUpdate = status?.copy() ?: Record(ListStatus.NONE, 0, 0, Id(series.id))
-        val presenter = getPresenter()
-
-        val episodes = emptyList<Int>().plus(Array(series.totalEpisodes + 1, Int::toInt))
-        progSpinner.setItems(episodes)
-        progSpinner.isEnabled = false
-
-        val scores = emptyList<Int>().plus(Array(11, Int::toInt))
-        scoreSpinner.setItems(scores)
-        scoreSpinner.isEnabled = false
-
-        val statuses = ListStatus.values().map { it.string }
-        statusSpinner.setItems(statuses)
-
-        progSpinner.setOnItemSelectedListener { view, position, id, item ->
-            posUpdate.episodesWatched = position
-            updateButton.isEnabled = presenter.isUpdateable(posUpdate)
-        }
-        scoreSpinner.setOnItemSelectedListener { view, position, id, item ->
-            posUpdate.score = position
-            updateButton.isEnabled = presenter.isUpdateable(posUpdate)
-        }
-        statusSpinner.setOnItemSelectedListener { view, position, id, item ->
-            val listStatus = ListStatus.values()[position]
-            posUpdate.listStatus = listStatus
-            if (listStatus == ListStatus.COMPLETED) {
-                posUpdate.episodesWatched = series.totalEpisodes
-                progSpinner.selectedIndex = series.totalEpisodes
-                progSpinner.isEnabled = false
-            } else if (listStatus == ListStatus.NONE || listStatus == ListStatus.PLAN_TO_WATCH) {
-                posUpdate.episodesWatched = 0
-                progSpinner.selectedIndex = 0
-                posUpdate.score = 0
-                scoreSpinner.selectedIndex = 0
-                progSpinner.isEnabled = false
-                scoreSpinner.isEnabled = false
-            } else {
-                progSpinner.isEnabled = true
-                scoreSpinner.isEnabled = true
-            }
-            updateButton.isEnabled = presenter.isUpdateable(posUpdate)
-        }
-
-        updateButton.setOnClickListener {
-            presenter.onUpdateButtonClicked(posUpdate)
-        }
-    }
-
-    private fun setSpinnersSelected(status: Record?) {
-        if (status != null) {
-            val statuses = ListStatus.values().map { it.string }
-            progSpinner.selectedIndex = status.episodesWatched
-            scoreSpinner.selectedIndex = status.score
-            statusSpinner.selectedIndex = statuses.indexOf(status.listStatus.string)
-            scoreSpinner.isEnabled = status.listStatus != ListStatus.PLAN_TO_WATCH &&
-                    status.listStatus != ListStatus.NONE
-            progSpinner.isEnabled = scoreSpinner.isEnabled &&
-                    status.listStatus != ListStatus.COMPLETED
+                override fun onFailure(call: Call<Favourite>, t: Throwable?) {
+                    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            })
         }
     }
 
@@ -152,15 +116,11 @@ class SeriesPageController(args: Bundle?) : PagerChildController(args) {
         descriptionTextView.text = series.description?.replace("<br>", "")
         titleTextView.text = series.titleEnglish
         favFab.setImageResource(if (series.favourite ?: false) R.drawable.ic_menu_favourited else R.drawable.ic_menu_unfavourited)
-        initSpinners(series, status)
-        setSpinnersSelected(status)
     }
 
     override fun onItemUpdateableChanged(canUpdate: Boolean) {
-        view.series_detail_update_button.isEnabled = false
     }
 
     override fun onFavouriteChanged(favourite: Boolean) {
-        favFab.setImageResource(if (favourite) R.drawable.ic_menu_favourited else R.drawable.ic_menu_unfavourited)
     }
 }
